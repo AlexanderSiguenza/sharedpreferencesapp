3280d0b5d6aa3fb20971c3dec9d41f927fb23442
ALEXANDER ALBERTO SIGUENZA CAMPOS <alexander.siguenzac@claro.com.sv>
2023-03-18 10:08:26 -0600

    primer commit

---
+++ app/src/main/java/edu/udb/sharedpreferencesapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package edu.udb.sharedpreferencesapp
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val et1=findViewById<EditText>(R.id.et1)
+        val et2=findViewById<EditText>(R.id.et2)
+
+        val preferencias = getSharedPreferences("agenda", Context.MODE_PRIVATE)
+        val boton1=findViewById<Button>(R.id.boton1)
+
+        boton1.setOnClickListener{
+            val editor = preferencias.edit()
+            editor.putString(et1.text.toString(), et2.text.toString())
+            editor.commit()
+            Toast.makeText(this, "Datos grabados", Toast.LENGTH_LONG).show()
+            et1.setText("")
+            et2.setText("")
+        }
+
+        val boton2=findViewById<Button>(R.id.boton2)
+        boton2.setOnClickListener {
+            val datos = preferencias.getString(et1.text.toString(), "")
+            if (datos != null) {
+                if (datos.length == 0) {
+                    et2.setText("")
+                    Toast.makeText(this, "No existe dicho nombre en la agenda", Toast.LENGTH_LONG).show()
+                    et2.setText("No existe dicho nombre en la agenda")
+                }else {
+                    et2.setText(datos)
+                }
+            }
+        }
+   }
+}
